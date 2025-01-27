fa288b23e964c1a454c5ba12a56db2e0d06a294f
Stephen Curtis <steve.curtis106@gmail.com>
2025-01-26 21:07:36 -0700

    Basic example

---
+++ example1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/james')
+def hello_james():
+    return 'Hello, James!'
+
+@app.route('/tyler')
+def hello_tyler():
+    return 'Hello, Tyler!'
+
+if __name__ == '__main__':
+    app.run()
